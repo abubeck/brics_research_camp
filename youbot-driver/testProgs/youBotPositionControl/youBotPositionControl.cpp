0126658c12657fa23d1e6b0e277dfccdd1e02a4d
BRICS Research Camp <brics@rosser-pc.(none)>
2010-10-27 11:01:42 +0200

    Base position control

---
+++ youbot-driver/testProgs/youBotPositionControl/youBotPositionControl.cpp
@@ -0,0 +1,68 @@
+/* 
+ * File:   main.cpp
+ * Author: Özgür Sen
+ *
+ * Created on 24. Oktober 2010, 17:32
+ */
+
+#include <cstdlib>
+#include <youBotApi.h>
+
+using namespace std;
+using namespace youbot;
+
+YouBotApi* youBot;
+
+void driveTo(double x, double y, double theta) {
+	double maxTransSpeed = 0.1, maxRotSpeed = 0.1;
+
+	while (true) {
+		double curX, curY, curTheta;
+		youBot->getBasePositionCartesian(curX, curY, curTheta);
+
+		double distX = x - curX, distY = y - curY, distTheta = theta - curTheta;
+
+		double time = max( fabs(distX / maxTransSpeed), max(fabs(distY / maxTransSpeed), fabs(distTheta / maxRotSpeed)));
+		double velWorldX = distX / time, velWorldY = distY / time, velTheta = -distTheta / time;
+		double velX = velWorldX * cos(-theta) + velWorldY * sin(-theta);
+		double velY = -velWorldY * sin(-theta) + velWorldY * cos(-theta);
+		printf("x: %f, y: %f, theta: %f, remaining time: %f\n", curX, curY, curTheta, time);
+		printf("velX: %f, velY: %f, velTheta: %f\n", velX, velY, velTheta);
+		youBot->setBaseVelocitiesCartesian(velX, velY, velTheta);
+		usleep(4000);
+		if(time < 0.01) break;
+	}
+	youBot->setBaseVelocitiesCartesian(0, 0, 0);
+}
+
+int main(int argc, char** argv) {
+
+	//youBotApi
+	int semaphoreKey = 222;
+
+	if (argc != 1) {
+		semaphoreKey = atoi(argv[1]);
+	}
+
+	youBot = new YouBotApi("/tmp/youBotMemMapFile", semaphoreKey);
+	//	youBot->setBaseVelocitiesCartesian(0,0,0);
+	//	sleep(1);
+	//	double vx, vy, vtheta;
+	//	youBot->getBaseVelocitiesCartesian(vx, vy, vtheta);
+	//	printf("x: %f, y: %f, theta: %f\n", vx, vy, vtheta);
+	//	printf("rpm: %i\n", youBot->getActualVelocity(0));
+	//	printf("rpm: %i\n", youBot->getActualVelocity(1));
+	//	printf("rpm: %i\n", youBot->getActualVelocity(2));
+	//printf("rpm: %i\n", youBot->getActualVelocity(3));
+	//return 0;
+	/* wheels are 0..3 */
+	for (int i = 0; i < 4; i++) {
+		youBot->setControllerMode(i, 2); //2: velocity, 1: position, 3: move by hand
+		youBot->setMotorPositionOrSpeed(i, 0);
+	}
+
+	driveTo(0,0,0);
+
+	return 0;
+}
+
