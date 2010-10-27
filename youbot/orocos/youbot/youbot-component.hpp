8ea5fd9d51d7067b47c6dda64ce1e0e97fe0ad4e
Peter Soetens <peter@thesourceworks.com>
2010-10-27 09:01:55 +0200

    youbot: initial ik test code in orocos component.
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- youbot/orocos/youbot/youbot-component.hpp
+++ youbot/orocos/youbot/youbot-component.hpp
@@ -7,6 +7,7 @@
 #include <youBotApi.h>
 #include "youbot_chain.hpp"
 #include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolvervel_wdls.hpp>
 #include <cmath>
 
 using namespace std;
@@ -22,7 +23,7 @@ typedef struct{
   double minJointValue;
   //double minAxisValue;
   bool negative;
-
+  double rpm_to_rad;
 } YouJoint;
 
 
@@ -45,6 +46,15 @@ class Youbot
        return jointValue/360*(joints[jointID].gearRatio*encoderSteps);
    }
 
+   //Convert from encoder speeds to joint speeds in radians per second
+   double encoderSpeedToRadPerSec(double encoderSpeed, int jointID){
+	   return encoderSpeed*joints[jointID].rpm_to_rad;
+   }
+
+   //Convert from joint speeds in radians per second to encoder speeds
+   double radPerSecToEncoderSpeed(double radPerSec, int jointID){
+   	   return radPerSec/joints[jointID].rpm_to_rad;
+   }
 
    // Convert from Joint value to axis absolute value
    int getAxisAbsolutePosition(double jointPosition, int jointID){
@@ -65,8 +75,11 @@ class Youbot
 
    Chain chain;
    ChainFkSolverPos_recursive *solver;
+   ChainIkSolverVel_wdls *iksolver;
    JntArray q;
    Frame eef;
+   JntArray qdot;
+   Eigen::MatrixXd Mq;
 
 public:
     Youbot(string const& name)
@@ -75,17 +88,23 @@ public:
     {
         std::cout << "Youbot constructed !" <<std::endl;
 
-        addOperation("move",&Youbot::setAxisPosition, this)
+        addOperation("move",&Youbot::move, this)
         .doc("move axis to given position").arg("axis","axis 0..4").arg("degrees","degrees");
+
+        addOperation("setSpeed",&Youbot::setSpeed, this)
+        .doc("move axis at given speed").arg("axis","axis 0..4").arg("radPerSec","radians");
 
         addOperation("openGripper",&Youbot::openGripper, this);
         addOperation("closeGripper",&Youbot::closeGripper, this);
+
+        addOperation("speedTest",&Youbot::speedTest, this);
 
         addProperty("eef",eef);
 
         chain = getYouBotKinematicChain();
         q.resize(8);
         solver = new ChainFkSolverPos_recursive(chain);
+        iksolver = new ChainIkSolverVel_wdls(chain);
 
         this->configure();
     }
@@ -99,6 +118,7 @@ public:
     	joints[0].minJointValue = -169;
     	joints[0].maxJointValue = 169;
     	joints[0].negative = true;
+    	joints[0].rpm_to_rad = -0.0783;
     	//joints[0].minAxisValue = -585659;
 
     	// Joint 1 parameters
@@ -107,6 +127,7 @@ public:
     	joints[1].minJointValue = -65;
     	joints[1].maxJointValue = 90;
     	joints[1].negative = true;
+    	joints[1].rpm_to_rad = -0.0807;
     	//joints[1].minAxisValue = -268741;
 
     	// Joint 2 parameters
@@ -115,6 +136,7 @@ public:
     	joints[2].minJointValue = -151;
     	joints[2].maxJointValue = 146;
     	joints[2].negative = false;
+    	joints[2].rpm_to_rad = 0.0863;
     	//joints[2].minAxisValue = -325596;
 
     	// Joint 3 parameters
@@ -123,6 +145,7 @@ public:
     	joints[3].minJointValue = -102.5;
     	joints[3].maxJointValue = 102.5;
     	joints[3].negative = true;
+    	joints[3].rpm_to_rad = -0.0863;
     	//joints[3].minAxisValue = -162930;
 
     	// Joint 4 parameters
@@ -131,6 +154,7 @@ public:
     	joints[4].minJointValue = -167.5;
     	joints[4].maxJointValue = 167.5;
     	joints[4].negative = true;
+    	joints[4].rpm_to_rad = -0.0825;
     	//joints[4].minAxisValue = 131697;
 
     	/*// Compute max and min joints value
@@ -138,9 +162,9 @@ public:
     	  joints[i].minAxisValue = jointValueToEncoderValue(joints[i].minJointValue, joints[i].jointID);
     	  joints[i].maxAxisValue = jointValueToEncoderValue(joints[i].maxJointValue, joints[i].jointID);
     	  }*/
+    	this->getActivity()->setPeriod(0.1);
 
-
-    	// steer zero velocities:
+    	// steer zero velocities (wheels):
     	for(int i=0;i < 4; i++)
     	{
     		youBot.setControllerMode(i,2);  //2: velocity, 1: position, 3: move by hand
@@ -156,11 +180,58 @@ public:
     	youBot.setAxisPosition(3, getAxisAbsolutePosition(0,3));
     	youBot.setAxisPosition(4, getAxisAbsolutePosition(0,4));
 
+    	/* Disable base: */
+    	//Mq = Eigen::
+    	//setWeightJS(Mq);
+
         return true;
     }
 
-    bool setAxisPosition(int axis, double degrees) {
+//    bool movePose(const KDL::Frame& eef) {
+//    	target_frame = eef;
+//    }
+
+    bool move(int axis, double degrees) {
+    	youBot.setControllerMode(axis+4,1);
     	return youBot.setAxisPosition(axis, getAxisAbsolutePosition(degrees,axis)) == 0;
+    }
+
+    bool setSpeed(int axis, double radPerSec) {
+    	youBot.setControllerMode(axis+4,2);
+    	return youBot.setMotorPositionOrSpeed(axis+4, radPerSecToEncoderSpeed(axis, radPerSec)) == 0;
+    }
+
+    bool speedTest(int axis, int speed) {
+    	double startPos = getJointAbsolutePosition(youBot.getAxisPosition(axis), axis) / 180.* M_PI;
+    	youBot.setControllerMode(axis+4,2);  //2: velocity, 1: position, 3: move by hand
+    	youBot.setMotorPositionOrSpeed(axis+4,speed);
+    	sleep(10);
+    	double endPos = getJointAbsolutePosition(youBot.getAxisPosition(axis), axis) / 180.* M_PI;
+    	youBot.setMotorPositionOrSpeed(axis+4,0);
+    	youBot.setControllerMode(axis+4,1);
+    	youBot.setAxisPosition(axis, getAxisAbsolutePosition(0,axis));
+    	cout << axis << ": " << endPos - startPos << endl;
+    }
+
+    bool getJointAngles(){
+    	q(0) = 0;
+    	q(1) = 0;
+    	q(2) = 0;
+    	for(int i=3; i < q.rows(); ++i) {
+    		q(i) = getJointAbsolutePosition(youBot.getAxisPosition(i-3), i-3) / 180.* M_PI;
+    	}
+    }
+
+    bool moveToCartPos(const KDL::Frame& desired_eef, const KDL::Twist& eef_vel){
+    	getJointAngles();
+    	solver->JntToCart(q,eef); //FK: get end effector pos
+    	Twist del_eef = diff(desired_eef, eef);
+    	Twist del_vel = diff(eef_vel, del_eef);
+        iksolver->CartToJnt(q, del_vel, qdot); //IK: get joint velocities, from current joint angles and desired end effector velocity
+        for(int i=3; i < qdot.rows(); ++i) {
+        	setSpeed(i, qdot(i));
+        }
+
     }
 
     void closeGripper() {
@@ -175,12 +246,15 @@ public:
         std::cout << "Youbot started !" <<std::endl;
 
         //
-        this->getActivity()->setPeriod(0.1);
+
         return true;
     }
 
     void updateHook() {
         //std::cout << "Youbot executes updateHook !" <<std::endl;
+
+
+#if 0
         q(0) = 0;
         q(1) = 0;
         q(2) = 0;
@@ -189,7 +263,10 @@ public:
         	cout << " (" << q(i) * 180 / M_PI <<", "<< youBot.getAxisPosition(i-3) << ") ";
         }
         cout << endl;
-        solver->JntToCart(q,eef);
+        solver->JntToCart(q,eef); //FK: get end effector pos
+#endif
+
+        //moveToCartPos();
     }
 
     void stopHook() {
